7092f33b003feaf9a4c1c568fd7b8d1797884d3b
taran_1 <taran_1@mail.ru>
2025-03-24 23:36:16 +0700

    main

--- hello.sh
+++ hello.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 if [ "$(id -u)" -ne 0 ];then
-	echo "Войдите с sudo ./hello.sh"
+	echo "Введите число"
 	exit 1
 fi
 name=$USER
 echo $NAME
 path=$(which firefox-esr)
+pathF=$(which gcc)
+pathC=$(which VS code)
 sudo chmod 000 $path
+sudo chmod 000 $pathF
+sudo chmod 000 $pathC
